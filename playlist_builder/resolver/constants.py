6acf54c63da1c98709e5b6a83bdd0a212d394efa
DrCeylon <73071737+DrCeylon@users.noreply.github.com>
2026-06-29 09:47:55 -0400

    Sprint 2.3: tune resolver candidate constants

--- playlist_builder/resolver/constants.py
+++ playlist_builder/resolver/constants.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 
 FIELD_DELIMITER = "\x1e"
+CANDIDATE_DELIMITER = "\x1d"
 RESULT_DELIMITER = "\x1f"
 MAX_QUERY_VARIANTS = 6
+MAX_CANDIDATES_PER_TRACK = 12
 MIN_ACCEPTABLE_SCORE = 55
 
 _GENERIC_SECTIONS = frozenset({"playlist", "generated", "discovery"})
